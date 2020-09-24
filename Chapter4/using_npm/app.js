cb9676eceaba5af7d97d5fbdff632db187f53ce5
zhuanghaixin <649308293@qq.com>
2020-09-24 21:04:12 +0800

    feat(Node-Udemy): 学习Chapter6,学习template engines模版引擎pug

--- Chapter4/using_npm/app.js
+++ Chapter4/using_npm/app.js
@@ -1,6 +1,10 @@
+
+
 const http=require('http');
 const routes=require('./routes')
 console.log(routes.someText)
 const server=http.createServer(routes.handler)
 server.listen(3000)
+
+
 
---
+++ Chapter6/dynamic_content_pug/routes/admin.js
@@ -0,0 +1,37 @@
+const express=require('express')
+const path=require('path')
+const router=express.Router()
+
+const products=[]
+
+router.get('/add-product',(req,res,next)=>{
+    console.log('In the middleware!')
+    // res.sendFile(path.join(__dirname,'../','views','add-product.html'))
+    res.render('add-product',{pageTitle:'Add Product',path:'/admin/add-product'})
+});
+//多级路由
+// router.get('/admin/add-product',(req,res,next)=>{
+//     console.log('In the middleware!')
+//     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Products</button></form>')
+//
+// });
+router.post('/add-product',(req,res) =>{
+    console.log(req.body)
+    products.push({title:req.body.title})
+
+    res.redirect('/')
+
+})
+
+
+// router.post('/admin/product',(req,res) =>{
+//     console.log(req.body)
+//     res.redirect('/')
+//
+// })
+
+
+// module.exports=router
+
+exports.routes=router
+exports.products=products
