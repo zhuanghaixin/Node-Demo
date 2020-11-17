0738b15ad3781b3888c106f1db16e93d69c3ecfc
zhuanghaixin <649308293@qq.com>
2020-11-17 22:29:53 +0800

    feat(王者荣耀系统):完成验证码功能✅

--- WZRY_HERO/app.js
+++ WZRY_HERO/app.js
@@ -23,18 +23,21 @@ const fileUpload = require('express-fileupload')
 const session = require('express-session'); //导入express-session中间件
 
 //4.4.2 cookie-session中间件：用户会话状态保持   给req添加session属性
-// const cookieSession = require('cookie-session');
+const cookieSession = require('cookie-session');
 
 //4.5 A MYSQL session 存储
-const MySQLStore = require('express-mysql-session')(session);
-const options = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'zhx199710085470',
-    database: 'wzry'
-};
-const  sessionStore = new MySQLStore(options);
+// const MySQLStore = require('express-mysql-session')(session);
+// const options = {
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'zhx199710085470',
+//     database: 'wzry'
+// };
+// const  sessionStore = new MySQLStore(options);
+
+
+
 
 // 使用中间件
 //4.1
@@ -46,13 +49,13 @@ app.use(fileUpload())
 
 
 //4.4.1
-app.use(session({
-    key: 'session_cookie_name',
-    secret: 'session_cookie_secret',
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false
-}));
+// app.use(session({
+//     key: 'session_cookie_name',
+//     secret: 'session_cookie_secret',
+//     store: sessionStore,
+//     resave: false,
+//     saveUninitialized: false
+// }));
 //4.4.2
 // app.use(cookieSession({
 //     name: 's_id',
@@ -62,6 +65,7 @@ app.use(session({
 //     maxAge: 7 * 24 * 60 * 60 * 1000 // 24 hours
 // }));
 
+//4.5
 //5.导入数据库
 const sequelize = require('./utils/database')
 
@@ -74,10 +78,14 @@ const sequelize = require('./utils/database')
 const heroRoutes = require('./routes/hero')
 //6.2 鉴权路由
 const authRoutes = require('./routes/auth')
+//6.3 验证码路由
+const captchaRoute=require('./routes/captcha')
 //使用路由
 app.use('/hero', heroRoutes)
 //使用路由
 app.use(authRoutes)
+//使用路由
+app.use(captchaRoute)
 
 //6.x 404路由
 const errorController = require('./controllers/error')
